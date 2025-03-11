ecf4e21ec750e204a4fd0a65bc8d42a803ce7f75
Ethan Keshishian <keshishianethan@gmail.com>
2025-03-11 13:41:24 -0700

    Update mappings.lua to include Colemak nvim and tmux navigation

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -6,12 +6,25 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("i", "fp", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-\\>', ':TmuxNavigatePrevious<CR>', { noremap = true, silent = true })
+-- Uncomment for Querty tmux navigation
+-- vim.keymap.set('n', '<C-h>', ':TmuxNavigateLeft<CR>', { noremap = true, silent = true })
+-- vim.keymap.set('n', '<C-j>', ':TmuxNavigateDown<CR>', { noremap = true, silent = true })
+-- vim.keymap.set('n', '<C-k>', ':TmuxNavigateUp<CR>', { noremap = true, silent = true })
+-- vim.keymap.set('n', '<C-l>', ':TmuxNavigateRight<CR>', { noremap = true, silent = true })
+-- vim.keymap.set('n', '<C-\\>', ':TmuxNavigatePrevious<CR>', { noremap = true, silent = true })
 
+-- Colemak nvim and tmux window navigation
+vim.keymap.set('n', '<C-left>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-down>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-up>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-right>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<C-left>', ':TmuxNavigateLeft<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-down>', ':TmuxNavigateDown<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-up>', ':TmuxNavigateUp<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-right>', ':TmuxNavigateRight<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-\\>', ':TmuxNavigatePrevious<CR>', { noremap = true, silent = true })
