ed3471ffc739691825cdbd7c111184cda627a4cb
Ethan Keshishian <keshishianethan@gmail.com>
2025-03-13 22:30:43 -0700

    Added static-ls to lspconfig

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -28,3 +28,21 @@ lspconfig.jsonls.setup {
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
 }
+
+if not configs.static_ls then
+  configs.static_ls = {
+    default_config = {
+      cmd = { "static-ls" },
+      filetypes = { "haskell" },
+      root_dir = util.root_pattern("*.cabal", "stack.yaml", "cabal.project", "package.yaml", "hie.yaml"),
+      single_file_support = false,
+      settings = {},
+    },
+  }
+end
+
+lspconfig.static_ls.setup({
+  autostart = true,
+  capabilities = nvlsp.capabilities,
+  on_attach = nvlsp.on_attach,
+})
