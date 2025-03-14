c69d4c81eb66fcc80c6b747e9b7941c7cc905305
Ethan Keshishian <keshishianethan@gmail.com>
2025-03-13 23:03:04 -0700

    Adding mapping for vim diagnostics

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -7,7 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "fp", "<ESC>")
+
 map("n", "<leader>fr", "<cmd>Telescope resume<CR>", { desc = "telescope resume" })
+map("n", "<leader>fe", "<cmd>Telescope diagnostics<CR>", { desc = "telescope errors" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
